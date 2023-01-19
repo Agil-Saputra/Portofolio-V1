49ea64f1cf6613164ac78b9a857fc52ce1bca053
Agil <88566690+Agil-Saputra@users.noreply.github.com>
2023-01-19 21:00:17 +0700

    add About section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
-import Navbar from './components/navbar'
-import Hero from './components/hero'
+import Navbar from "./components/navbar";
+import Hero from "./components/hero";
+import About from "./components/about";
 function App() {
   return (
-    <div className="App">
-  <Navbar/>
-  <Hero />
+    <div className="">
+      <Navbar />
+      <main className="md:px-[5.5rem] px-[2.75rem]">
+        <Hero />
+        <About />
+      </main>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
