44ecabd41fe2a6252fb383cdba869a201259b996
Agil <88566690+Agil-Saputra@users.noreply.github.com>
2023-04-01 14:15:07 +0700

    fix resume button bug

--- src/components/ui/button.jsx
+++ src/components/ui/button.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function button({icon, title, link}) {
   return (
-    <a className="btn my-[1rem] shadow-xl md:shadow-2xl max-md:text-[13px]" href={link} target='_blank'>
+    <a className="btn my-[1rem] shadow-md md:shadow-lg max-md:text-[13px]" href={link} target='_blank'>
     <span className="button_top">{title}{icon}
     </span>
     </a>
